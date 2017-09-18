22e23a310ec20afc3dc807aee4b185eaf9fcee11
kshitijkorde <kshi.korde@gmail.com>
2017-09-18 12:50:34 +0100

    Adding Adapter Description

--- Adapter.cpp
+++ Adapter.cpp
@@ -1,3 +1,10 @@
+/*
+Ref: http://www.bogotobogo.com/DesignPatterns/adapter.php
+Intent:
+- Convert the interface of a class into another interface clients expect. 
+- Adapter (or Wrapper) lets classes work together that couldn't otherwise because of incompatible interfaces. 
+- Adapter pattern's motivation is that we can reuse existing software if we can modify the interface. */
+
 #include<bits/stdc++.h>
 
 using namespace std;
